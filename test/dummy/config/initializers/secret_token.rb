53f9f39f8acdddf7b02b1d519bf0d5a525174a97
Yaroslav Barkovskiy <y.barkovskij@unact.ru>
2020-01-28 17:04:51 +0300

    DEV-39252

--- test/dummy/config/initializers/secret_token.rb
+++ test/dummy/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = '9804084781dca158226e149901511123237227665a893efe8761db70bae34ea388062390b1e7209c9b18affc6aedbe453837c622031523dd00931580a8216acd'
+Dummy::Application.config.secret_key_base = '9804084781dca158226e149901511123237227665a893efe8761db70bae34ea388062390b1e7209c9b18affc6aedbe453837c622031523dd00931580a8216acd'
